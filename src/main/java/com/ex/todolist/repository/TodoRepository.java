22f3288e60e7a8e36afe8361ae0e2e29045aba20
guseoh <guseoh@naver.com>
2025-07-16 16:46:23 +0900

    feat: Paging Add

--- src/main/java/com/ex/todolist/repository/TodoRepository.java
+++ src/main/java/com/ex/todolist/repository/TodoRepository.java
@@ -1,11 +1,13 @@
 package com.ex.todolist.repository;
 
 import com.ex.todolist.entity.TodoList;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface TodoRepository extends JpaRepository<TodoList, Long> {
 
-    List<TodoList> findByTaskContaining(String keyword);
+    Page<TodoList> findByTaskContaining(String keyword, Pageable pageable);
 }
